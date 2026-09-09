7813a2f32e797350dbc8487f091d77313e1a4b43
moralesryan <rmoralesaguilar@byupathway.edu>
2026-09-09 12:56:33 -0600

    fix: update SSL configuration in PostgreSQL connection pool to handle unauthorized certificates

--- public/src/models/db.js
+++ public/src/models/db.js
@@ -14,9 +14,10 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 /**
  * Common SSL Issue:
  *

3acbca0feee356f29af99ab92cb3a349d746eea2
moralesryan <rmoralesaguilar@byupathway.edu>
2026-09-14 23:07:05 -0600

    feat: add middleware to log incoming requests in development mode

--- server.js
+++ server.js
@@ -30,6 +30,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public/src/views'))
 
 
+// Middleware to log all incoming requests
+app.use((req, res, next) => {
+  if (NODE_ENV === 'development') {
+    console.log(`${req.method} ${req.url}`);
+  }
+  next(); // Pass control to the next middleware or route
+});
+
+
 /**
  * Routes
  */

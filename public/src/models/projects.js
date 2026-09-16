646fc24dd65aed7c16165f86fa74d884d03e2727
moralesryan <rmoralesaguilar@byupathway.edu>
2026-09-15 23:20:27 -0600

    feat: implement organization details page with associated projects and update organizations list to link to details

--- public/src/models/projects.js
+++ public/src/models/projects.js
@@ -15,4 +15,24 @@ const getAllProjects = async () => {
     return result.rows;
 }
 
-export { getAllProjects }
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+      SELECT
+        project_id,
+        organization_id,
+        title,
+        description,
+        location,
+        date
+      FROM service_projects
+      WHERE organization_id = $1
+      ORDER BY date;
+    `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+export { getAllProjects, getProjectsByOrganizationId };
